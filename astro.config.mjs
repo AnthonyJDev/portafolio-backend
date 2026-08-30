e51f9cc7d5347379cad0119a84dab41cdacf2963
Anthony <anthony4lx@gmail.com>
2026-08-30 00:41:19 -0500

    Configurar despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Jaydnnx.github.io',
+  base: '/portafolio-backend',
   integrations: [react()],
 });
